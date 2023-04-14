48d1ed25ae293f3fa548054975b37a7c6cde127d
Cheree <71246795+Chere3@users.noreply.github.com>
2023-04-14 15:08:21 -0600

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata = {
@@ -12,7 +13,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <Script id="clarityManager" strategy='afterInteractive' dangerouslySetInnerHTML={{__html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}")`}}>
+
+      </Script>
+      <body>
+        {children}
+        </body>
     </html>
   )
 }
