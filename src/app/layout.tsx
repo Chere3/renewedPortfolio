fa2254d3dba99257ef753930deb2cb6074b71347
Cheree <71246795+Chere3@users.noreply.github.com>
2023-04-15 14:01:32 -0600

    chore: added first makeup content for the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,30 @@
-import Script from 'next/script'
-import './globals.css'
+import Script from "next/script";
+import "./globals.css";
 
 export const metadata = {
-  title: 'Portfolio | Aún en construcción',
-  description: 'Chere3',
-}
+  title: "Portfolio | Aún en construcción",
+  description: "Chere3",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="es">
-      <Script id="clarityManager" strategy='afterInteractive' dangerouslySetInnerHTML={{__html: `(function(c,l,a,r,i,t,y){
+      <Script
+        id="clarityManager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}")`}}>
-
-      </Script>
-      <body>
-        {children}
-        </body>
+    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}")`,
+        }}
+      ></Script>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,38 @@
+// @ts-check
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-     colors: {
-      'primary': '#f6fafd',
-      'secondary': '#000000',
-      'tertiary': '#F2F2F2',
-
-     }
+      colors: {
+        primary: "#df631b",
+        secondary: "#fffff",
+        "primary-light": "#f7b38c",
+        "primary-dark": "#b23c0d",
+        "secondary-light": "#ffffff",
+        "secondary-dark": "#cccccc",
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+        serif: ["Roboto", "serif"],
+        mono: ["Roboto", "monospace"],
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+      },
     },
   },
   plugins: [],
-}
+};
